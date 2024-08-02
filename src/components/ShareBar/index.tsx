f25841b5169c5c3579dee18cb0429d93a9c8623c
jaceleedev <jaceleedev@gmail.com>
2024-08-02 14:31:35 +0900

    feat(ShareBar): :iphone: add responsive design and tooltip arrow for desktop view

--- src/components/ShareBar/index.tsx
+++ src/components/ShareBar/index.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 function ShareBar() {
   return (
-    <div className="flex items-center w-[327px] h-16 px-8 py-[22px] rounded-b-[10px] bg-dark-grayish-blue">
+    <div className="relative flex items-center w-[327px] px-8 py-[22px] rounded-b-[10px] bg-dark-grayish-blue md:w-[248px] md:py-[18px] md:rounded-[10px]">
       <span className="text-action mr-[21px]">SHARE</span>
       <div className="flex items-center gap-4">
         <a href="#">
@@ -31,6 +31,8 @@ function ShareBar() {
           />
         </a>
       </div>
+      {/* Triangle shape for the tooltip arrow (visible only on desktop) */}
+      <div className="hidden absolute left-1/2 bottom-0 w-0 h-0 border-l-[12px] border-r-[12px] border-t-[12px] border-solid border-t-dark-grayish-blue border-l-transparent border-r-transparent transform -translate-x-1/2 translate-y-full md:block" />
     </div>
   );
 }
