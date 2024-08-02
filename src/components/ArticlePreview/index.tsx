b92920b8bfa2e8cfcc2d777a4c31b685e07e0e30
jaceleedev <jaceleedev@gmail.com>
2024-08-02 14:32:35 +0900

    feat(ArticlePreview): :iphone: add responsive layout for desktop view

--- src/components/ArticlePreview/index.tsx
+++ src/components/ArticlePreview/index.tsx
@@ -13,22 +13,22 @@ function ArticlePreview() {
   }
 
   return (
-    <article className="relative grid grid-cols-1 grid-rows-[200px_auto] w-[327px] rounded-[10px] bg-white shadow-[0px_40px_40px_-10px_rgba(201,213,225,0.50)]">
-      <figure className="relative h-[200px]">
+    <article className="relative grid grid-cols-1 grid-rows-[200px_auto] w-[327px] rounded-[10px] bg-white shadow-[0px_40px_40px_-10px_rgba(201,213,225,0.50)] md:grid-cols-[285px_auto] md:grid-rows-1 md:w-[730px]">
+      <figure className="relative h-[200px] md:h-[280px]">
         <Image
           src="/images/drawers.jpg"
           alt="drawers"
           fill
           priority
-          className="object-cover object-[center_25%] rounded-t-[10px]"
+          className="object-cover object-[center_25%] rounded-t-[10px] md:rounded-tr-none md:rounded-bl-[10px]"
         />
       </figure>
-      <div className="pt-9 px-8 pb-5">
+      <div className="pt-9 px-8 pb-5 md:px-10 md:py-8">
         <h1 className="text-heading mb-3">
           Shift the overall look and feel by adding these wonderful touches to
           furniture in your home
         </h1>
-        <p className="text-body mb-8">
+        <p className="text-body mb-8 md:mb-5">
           Ever been in a room and felt like something was missing? Perhaps it
           felt slightly bare and uninviting. I’ve got some simple tips to help
           you make any room feel complete.
@@ -54,7 +54,7 @@ function ArticlePreview() {
           />
         </div>
         {isShareBarOpen && (
-          <div className="absolute left-0 bottom-0">
+          <div className="absolute left-0 bottom-0 md:left-3/4 md:translate-x-[2.3px] md:bottom-1/3">
             <ShareBar />
           </div>
         )}
