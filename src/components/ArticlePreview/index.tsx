23b89b6c9ad29247848b6977609bd4f951042517
jaceleedev <jaceleedev@gmail.com>
2024-08-02 17:23:22 +0900

    refactor(ArticlePreview): :recycle: enhance accessibility and semantic structure

--- src/components/ArticlePreview/index.tsx
+++ src/components/ArticlePreview/index.tsx
@@ -14,15 +14,15 @@ function ArticlePreview() {
 
   return (
     <article className="relative grid grid-cols-1 grid-rows-[200px_auto] w-[327px] rounded-[10px] bg-white shadow-[0px_40px_40px_-10px_rgba(201,213,225,0.50)] md:grid-cols-[285px_auto] md:grid-rows-1 md:w-[730px]">
-      <figure className="relative h-[200px] md:h-[280px]">
+      <header className="relative h-[200px] md:h-[280px]">
         <Image
           src="/images/drawers.jpg"
-          alt="drawers"
+          alt="Stylish drawers in a room setting"
           fill
           priority
           className="object-cover object-[center_25%] rounded-t-[10px] md:rounded-tr-none md:rounded-bl-[10px]"
         />
-      </figure>
+      </header>
       <div className="pt-9 px-8 pb-5 md:px-10 md:py-8">
         <h1 className="text-heading mb-3">
           Shift the overall look and feel by adding these wonderful touches to
@@ -33,7 +33,7 @@ function ArticlePreview() {
           felt slightly bare and uninviting. I’ve got some simple tips to help
           you make any room feel complete.
         </p>
-        <div className="flex justify-between items-center">
+        <footer className="flex justify-between items-center">
           <div className="flex gap-4">
             <Image
               src="/images/avatar-michelle.jpg"
@@ -44,20 +44,27 @@ function ArticlePreview() {
               className="rounded-full"
             />
             <div>
-              <p className="text-author">Michelle Appleton</p>
-              <p className="text-date">28 Jun 2020</p>
+              <p className="text-author">
+                <span className="sr-only">Author: </span>Michelle Appleton
+              </p>
+              <p className="text-date">
+                <span className="sr-only">Publication date: </span>28 Jun 2020
+              </p>
             </div>
           </div>
           <ShareButton
             isShareBarOpen={isShareBarOpen}
             onShareBarToggle={handleShareBarToggle}
           />
-        </div>
-        {isShareBarOpen && (
-          <div className="absolute left-0 bottom-0 md:left-3/4 md:translate-x-[2.3px] md:bottom-1/3">
-            <ShareBar />
-          </div>
-        )}
+        </footer>
+        <dialog
+          open={isShareBarOpen}
+          id="share-bar"
+          aria-label="Share options"
+          className="absolute left-0 bottom-0 md:left-3/4 md:translate-x-[2.3px] md:bottom-1/3"
+        >
+          <ShareBar />
+        </dialog>
       </div>
     </article>
   );
