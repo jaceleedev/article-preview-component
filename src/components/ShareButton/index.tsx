29263ecac8a48350feba49562db8321599ffcefb
jaceleedev <jaceleedev@gmail.com>
2024-08-02 17:27:41 +0900

    refactor(ShareButton): :recycle: improve accessibility with dynamic aria-label

--- src/components/ShareButton/index.tsx
+++ src/components/ShareButton/index.tsx
@@ -6,7 +6,10 @@ interface ShareButtonProps {
   onShareBarToggle: () => void;
 }
 
-function ShareButton({ isShareBarOpen, onShareBarToggle }: ShareButtonProps) {
+function ShareButton({
+  isShareBarOpen,
+  onShareBarToggle,
+}: Readonly<ShareButtonProps>) {
   return (
     <button
       onClick={onShareBarToggle}
@@ -15,6 +18,9 @@ function ShareButton({ isShareBarOpen, onShareBarToggle }: ShareButtonProps) {
           ? 'bg-desaturated-dark-blue translate-y-2 md:translate-y-0'
           : 'bg-light-grayish-blue'
       }`}
+      aria-expanded={isShareBarOpen}
+      aria-controls="share-bar"
+      aria-label={isShareBarOpen ? 'Close share options' : 'Open share options'}
     >
       <ShareIcon fill={`${isShareBarOpen ? '#FFFFFF' : '#6E8098'}`} />
     </button>
